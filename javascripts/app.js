a3bf4623ab736ffa23e84ce2c282b6588376cb5e
Beltran Rengifo <merloc@gmail.com>
2017-12-11 17:44:50 +0100

    version preliminar con bonus backwards y validacion

--- javascripts/app.js
+++ javascripts/app.js
@@ -16,18 +16,19 @@ console.log('dirección por defecto: ' + dir);
 console.log('posición por defecto X: ' + Xpos);
 console.log('posición por defecto Y: ' + Ypos);
 
-// without jQuery (doesn't work in older IEs)
+// cargar historia por defecto después del load inicial
 document.addEventListener('DOMContentLoaded', function() {
-    movementHistory('dirección inicial: ' + dir, rover);
-    movementHistory('posición inicial X: ' + Xpos, rover);
-    movementHistory('posición inicial Y: ' + Ypos, rover);
+    movementHistory('default D: ' + dir, rover);
+    movementHistory('default X: ' + Xpos, rover);
+    movementHistory('default Y: ' + Ypos, rover);
 }, false);
+
 /* ======================
 funciones de movimiento
 ====================== */
 function turnLeft(rover) {
     console.log('turnLeft / dirección previa: ' + dir);
-    movementHistory('turnLeft / dirección previa: ' + dir, rover);
+    // movementHistory('turnLeft D: ' + dir, rover);
     switch (dir) {
         case 'N':
             dir = 'W';
@@ -43,12 +44,12 @@ function turnLeft(rover) {
             break;
     }
     console.log('turnleft / dirección reasignada: ' + dir);
-    movementHistory('turnleft / dirección reasignada: ' + dir, rover);
+    movementHistory('turnLeft D: ' + dir, rover);
 }
 
 function turnRight(rover) {
     console.log('turnRight / dirección previa: ' + dir);
-    movementHistory('turnRight / dirección previa: ' + dir, rover);
+    // movementHistory('turnRight / previous dir: ' + dir, rover);
     switch (dir) {
         case 'N':
             dir = 'E';
@@ -64,34 +65,12 @@ function turnRight(rover) {
             break;
     }
     console.log('turnRight / dirección reasignada: ' + dir);
-    movementHistory('turnRight / dirección reasignada: ' + dir, rover);
+    movementHistory('turnRight D: ' + dir, rover);
 }
-
-// function moveForward(rover) {
-//     console.log('moveForward / posición previa: X ' + Xpos + ', Y ' + Ypos);
-//     movementHistory('moveForward / posición previa: X ' + Xpos + ', Y ' + Ypos, rover);
-//     switch (dir) {
-//         case 'N':
-//             Ypos += 1;
-//             break;
-//         case 'S':
-//             Ypos -= 1;
-//             break;
-//         case 'E':
-//             Xpos += 1;
-//             break;
-//         case 'W':
-//             Xpos -= 1;
-//             break;
-//     }
-//     console.log('moveForward / posición reasignada: X ' + Xpos + ', Y ' + Ypos);
-//     movementHistory('moveForward / posición reasignada: X ' + Xpos + ', Y ' + Ypos, rover);
-// }
-
 
 function moveForward(rover) {
     console.log('moveForward / posición previa: X ' + Xpos + ', Y ' + Ypos);
-    movementHistory('moveForward / posición previa: X ' + Xpos + ', Y ' + Ypos, rover);
+    // movementHistory('moveForward / previous pos: X ' + Xpos + ', Y ' + Ypos, rover);
     if ( dir === 'N' && Ypos < 10 ) {
         Ypos += 1;
     }
@@ -108,7 +87,29 @@ function moveForward(rover) {
         movementHistory('Rover is beyond limits',rover);
     }
     console.log('moveForward / posición reasignada: X ' + Xpos + ', Y ' + Ypos);
-    movementHistory('moveForward / posición reasignada: X ' + Xpos + ', Y ' + Ypos, rover);
+    movementHistory('moveForward X ' + Xpos + ', Y ' + Ypos, rover);
+}
+
+function moveBackward(rover) {
+    console.log('moveBackward / posición previa: X ' + Xpos + ', Y ' + Ypos);
+    // movementHistory('moveBackward / previous pos: X ' + Xpos + ', Y ' + Ypos, rover);
+    if ( dir === 'N' && Ypos > 0 ) {
+        Ypos -= 1;
+    }
+    else if ( dir === 'S' && Ypos < 10 ) {
+        Ypos += 1;
+    }
+    else if ( dir === 'E' && Xpos > 0 ) {
+        Xpos -= 1;
+    }
+    else if ( dir === 'W' && Xpos < 10 ) {
+        Xpos += 1;
+    }
+    else {
+        movementHistory('Rover is beyond limits',rover);
+    }
+    console.log('moveBackward / posición reasignada: X ' + Xpos + ', Y ' + Ypos);
+    movementHistory('moveBackward X ' + Xpos + ', Y ' + Ypos, rover);
 }
 
 /* ======================
@@ -122,11 +123,17 @@ function processTextSequence() {
         if (command === 'f') {
             moveForward(rover);
         }
-        if (command === 'l') {
+        else if (command === 'b') {
+            moveBackward(rover);
+        }
+        else if (command === 'l') {
             turnLeft(rover);
         }
-        if (command === 'r') {
+        else if (command === 'r') {
             turnRight(rover);
+        }
+        else {
+            modalAviso();
         }
     }
 }
@@ -142,6 +149,9 @@ window.addEventListener("keyup", function(event) {
     switch (key) {
         case 'ArrowUp':
             moveForward(rover);
+            break;
+        case 'ArrowDown':
+            moveBackward(rover);
             break;
         case 'ArrowLeft':
             turnLeft(rover);
@@ -181,6 +191,13 @@ function printTraveLog(rover) {
     travelLogContainer.appendChild(p);
 }
 
+/* ======================
+lanzar modal aviso
+====================== */
+function modalAviso() {
+    var modal = document.getElementById('modal-aviso')
+    modal.style.display = 'block';
+}
 /* ======================
 abort mission
 ====================== */
